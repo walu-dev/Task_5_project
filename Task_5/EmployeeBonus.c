85ab038d4032d9c502762835c3b8b9dbe43eeff0
walu-dev <jonathanenoch11@gmail.com>
2025-06-10 18:46:18 +0300

    Calculate final salary

--- Task_5/EmployeeBonus.c
+++ Task_5/EmployeeBonus.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include "EmployeeBonus.h"
 
+CalculateEmployeeBonus(){
+    double bonus = 0.05;
+    double WorkingYears, FinalSalary, BasicSalary;
+
+    // Prompt user for years of service and basic salary
+    printf("Enter years of service: ");
+    scanf("%lf", &WorkingYears);
+    printf("Enter basic salary: ");
+    scanf("%lf", &BasicSalary);
+
+    // Check if years of service exceed 5 and apply bonus
+    if(WorkingYears > 5){
+        FinalSalary = (BasicSalary*bonus) + BasicSalary;
+        printf("The final salary after bonus is: %.2lf\n",FinalSalary);
+    }else {
+        FinalSalary = BasicSalary;
+        printf("The Final Salary without bonus is: %.2lf\n");
+    }
+}
