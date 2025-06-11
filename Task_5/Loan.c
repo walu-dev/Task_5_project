d20a0cd51fcf5831bf89c9de12737314a7499ac3
walu-dev <jonathanenoch11@gmail.com>
2025-06-11 13:37:10 +0300

    Calculate and display loan eligibility

--- Task_5/Loan.c
+++ Task_5/Loan.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include "Loan.h"
 
+
+double loanEligibility(double EmployeeYears, double Salary){
+    // Get user input
+    printf("Enter your monthly salary (UGX): ");
+    scanf("%lf", &Salary);
+
+    printf("Enter your years of employment: ");
+    scanf("%d", &EmployeeYears);
+    if(Salary >= 5000 || EmployeeYears >= 2){
+        printf("You are eligible for a loan");
+    }else {
+         printf("You are not eligible for a loan");
+    }
+    return 0;
+}
--- Task_5/main.c
+++ Task_5/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "PurchaseAmount.h"
+#include "Loan.h"
 
+double EmployeeYears, Salary;
 int main()
 {
     //CalculatePurchaseAmount();
-    CalculateEmployeeBonus();
+    //CalculateEmployeeBonus();
+    loanEligibility(EmployeeYears, Salary);
     return 0;
 }
