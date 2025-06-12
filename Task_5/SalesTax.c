91e4171ba0342321ad9b44af6fae6c7dbaa15fbe
walu-dev <jonathanenoch11@gmail.com>
2025-06-12 13:02:16 +0300

    Calculate payable Amount

--- Task_5/SalesTax.c
+++ Task_5/SalesTax.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include "SalesTax.h"
 
+double CalculateAmountPayable(){
+    double taxRate, tax, purchaseAmount, payableAmount;
+    printf("Enter Purchase Amount:");
+    scanf("%lf", &purchaseAmount);
+
+    if(purchaseAmount < 500){
+        taxRate = 0.05;
+        printf("The applied tax rate is 0.05\n");
+    }else {
+        taxRate = 0.08;
+        printf("The applied tax rate is 0.08\n");
+    }
+
+    tax = purchaseAmount * taxRate;
+    payableAmount = purchaseAmount + tax;
+    return payableAmount;
+}
--- Task_5/main.c
+++ Task_5/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "PurchaseAmount.h"
 #include "Loan.h"
+#include "SalesTax.h"
 
 double EmployeeYears, Salary;
 int main()
@@ -9,5 +10,7 @@ int main()
     //CalculatePurchaseAmount();
     //CalculateEmployeeBonus();
     //loanEligibility(EmployeeYears, Salary);
+    double CalculatedAmountPayable = CalculateAmountPayable();
+    printf("The payable Amount is: %.2lf", CalculatedAmountPayable);
     return 0;
 }
